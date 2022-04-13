a77862af81070d746d34ffe9655537b69da983b0
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-13 03:13:57 +0300

    updated demo

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const date = document.querySelector('input').value
-  const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`
+  const url = `https://api.nasa.gov/planetary/apod?api_key=Ay0aS0fIuIfY32JkKfuRkk7L60eSPeXJz0PjgV4H&date=${date}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         document.querySelector('span').innerText = data.date
         document.querySelector('h2').innerText = data.title
-        document.querySelector('img').src = data.url
+        
         document.querySelector('p').innerText = data.explanation
+        if(data.media_type === 'image'){
+          document.querySelector('img').src = data.url
+        }else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
